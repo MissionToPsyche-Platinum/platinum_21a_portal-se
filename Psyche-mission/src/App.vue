5202dc25ea1ed18e527425996555b2ac9113d21b
Babiker Babiker <Bbabiker@asu.edu>
2026-03-01 21:16:38 -0800

    Task #36: Create Vue homepage

---
+++ Psyche-mission/src/App.vue
@@ -0,0 +1,14 @@
+<script>
+
+
+</script>
+
+<template>
+  <h1>Welcome to the Psyche mission's web-based game portal!</h1>
+<!--  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>-->
+</template>
+
+<style scoped></style>
