5962507238da4d407686183615a873afd2f867ef
Babiker Babiker <Bbabiker@asu.edu>
2026-03-01 21:41:27 -0800

    Updated homepage layout

--- Psyche-mission/src/App.vue
+++ Psyche-mission/src/App.vue
@@ -5,10 +5,7 @@
 
 <template>
   <h1>Welcome to the Psyche mission's web-based game portal!</h1>
-<!--  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>-->
+
 </template>
 
 <style scoped></style>
