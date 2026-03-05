d6c8169dc1881c8db69f929d5c066c031acca536
kstyers1 <kstyers1@asu.edu>
2026-03-04 17:32:56 -0800

    Add button to App.vue for testing GamePage

--- Psyche-mission/src/App.vue
+++ Psyche-mission/src/App.vue
@@ -1,10 +1,31 @@
 <script>
+import GamePage from './GamePage.vue';
 
+export default {
+  components: {
+    GamePage
+  },
+  data() {
+    return {
+      viewGamePage: false
+    }
+  },
+  methods: {
+    toggleGamePage() {
+      this.viewGamePage = !this.viewGamePage
+    }
+  }
+}
 
 </script>
 
 <template>
   <h1>Welcome to the Psyche mission's web-based game portal!</h1>
+  <button @click="toggleGamePage">view game page</button>
+
+  <div v-if="viewGamePage">
+    <GamePage />
+  </div>
 
 </template>
 
