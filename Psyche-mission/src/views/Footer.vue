d6cbf86dbb85319bf2eb9cb98f09a26cf0f78965
aghines <aghines@asu.edu>
2026-03-16 21:33:47 -0700

    Fixed App.vue to mount both Footer and NavBar

---
+++ Psyche-mission/src/views/Footer.vue
@@ -0,0 +1,40 @@
+<template>
+    <footer class="footer">
+        <a href="https://www.facebook.com/MissionToPsyche">
+            <i class="fab fa-facebook"></i>
+        </a>
+        <a href="https://www.instagram.com/missiontopsyche/">
+            <i class="fab fa-instagram"></i>
+        </a>
+        <a href="https://x.com/MissionToPsyche">
+            <i class="fab fa-x-twitter"></i>
+        </a>
+        <a href="https://www.youtube.com/channel/UC2BGcbPW8mxryXnjQcBqk6A">
+            <i class="fab fa-youtube"></i>
+        </a>
+    </footer>
+</template>
+
+<style scoped>
+.footer {
+    background: white;
+    padding: 20px;
+    text-align: center;
+}
+.footer i {
+    font-size: 50px;
+    padding: 10px;
+}
+.footer i.fa-facebook {
+    color: #1877f2;
+}
+.footer i.fa-instagram {
+    color: #e1306c;
+}
+.footer i.fa-x-twitter {
+    color: #1da1f2;
+}
+.footer i.fa-youtube {
+    color: #ff0000;
+}
+</style>
