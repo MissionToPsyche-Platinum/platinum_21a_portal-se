426c86c1060f45d44b2d431eef106f6a94cc5c1c
Babiker Babiker <Bbabiker@asu.edu>
2026-03-26 01:05:46 -0700

    Task #77: Add thumbnail preview to game card

--- Psyche-mission/src/components/GameLink.vue
+++ Psyche-mission/src/components/GameLink.vue
@@ -1,34 +1,64 @@
 <script>
 export default {
-    props: {
-        game: { type: Object },
-        isDark: { type: Boolean},
-    },
-    data() {
-        return {
-            dark: this.isDark,
-        };
-    },
+  props: {
+    game: { type: Object },
+    isDark: { type: Boolean},
+  },
+  data() {
+    return {
+      dark: this.isDark,
+    };
+  },
 };
 </script>
 
 <template>
-    <div :class="{'dark-mode': isDark}" class="game-link-card">
-        <h2>{{ game.title }}</h2>
-        <router-link :to="{ name: 'GamePage', params: {id: game.id }}">
-            <h3 :class="{'dark-link': isDark}" class="game-link">Play</h3>
-        </router-link>
+  <div :class="{'dark-mode': isDark}" class="game-link-card">
+
+    <div class="preview">
+
+<!--      show image if thumbnail exists and not "None"-->
+      <img
+          v-if="game.thumbnail && game.thumbnail !== 'None'"
+          :src="game.thumbnail"
+          class="media"
+          :alt="game.title"
+      />
+
     </div>
+
+    <h2>{{ game.title }}</h2>
+    <router-link :to="{ name: 'GamePage', params: {id: game.id }}">
+      <h3 :class="{'dark-link': isDark}" class="game-link">Play</h3>
+    </router-link>
+  </div>
 </template>
 
 <style scoped>
-    /*.game-link-card {
-        border: 2px solid black;
-        background-color:honeydew;
-        border-radius: 20px;
-        padding: 10px;
-        font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
-    }*/
+
+.preview {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 10px;
+}
+
+.media {
+  width: 100%;
+  max-width: 250px;
+  height: 150px;
+  object-fit: cover;
+  border-radius: 10px;
+}
+
+
+
+/*.game-link-card {
+    border: 2px solid black;
+    background-color:honeydew;
+    border-radius: 20px;
+    padding: 10px;
+    font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+}*/
 /*
     .dark-mode {
         border: 2px solid white;
