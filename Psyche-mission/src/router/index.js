04ef096cbc6d217601e87ab1c43e263e2646074e
aghines <aghines@asu.edu>
2026-08-31 16:32:40 -0700

    Creates login page UI

--- Psyche-mission/src/router/index.js
+++ Psyche-mission/src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import GamePage from "../views/GamePage.vue"
 import HomePage from "../views/HomePage.vue"
 import NotFound from "../views/NotFound.vue"
+import Login from "@/views/Login.vue";
 
 const routes = [
     {path: "/", component: HomePage},
     {path: "/gamepage", component: GamePage},
     {path: "/game/:id", name: "GamePage", component: GamePage, props: true},
+    {path: "/login", name: "Login", component: Login},
     {
         path: "/:path(.*)*", // Must be at end of routes. Checks for any url path that has not been defined above
         name: "NotFound",
---
+++ Psyche-mission/src/views/Login.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+    data() {
+        return {
+            username: "",
+            password: ""
+        }
+    },
+    methods: {
+        handleSubmit(event) {
+            //Replace with API call
+            event.preventDefault();
+            console.log("Login button pressed")
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>Login</h1>
+    <form @submit="handleSubmit" class="login-form">
+        <div class="input">
+            <input type="text" v-model="username" placeholder="Username" />
+            <input type="password" v-model="password" placeholder="Password" />
+            <button type="submit" class="submit-button">Login</button>
+            <router-link to="/signup" class="sign-up-button">Sign Up</router-link>
+        </div>
+    </form>
+</template>
+
+<style>
+h1 {
+    text-align: center;
+}
+.input {
+    display: flex;
+    flex-direction: column;
+    margin: 0 auto;
+    gap: 12px;
+    max-width: 300px;
+}
+.sign-up-button {
+    color: #330066;
+}
+</style>
