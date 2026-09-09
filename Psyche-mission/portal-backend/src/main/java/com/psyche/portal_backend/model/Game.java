62f83e65db25157d498fe4652689d117856074e1
Kurtis Styers <kstyers1@asu.edu>
2026-09-08 18:27:49 -0700

    Merge spring boot setup with dev-backend

---
+++ Psyche-mission/portal-backend/src/main/java/com/psyche/portal_backend/model/Game.java
@@ -0,0 +1,20 @@
+// package com.psyche.portal_backend.model;
+
+// public class Game {
+//     // private String id;  // will probably want to change to a number once DB is running
+//     // private String title;
+//     // private String genre;
+//     // private String difficulty;
+//     // private String description;
+//     // private String ageGroup;
+//     // private String className;
+//     // private String credits;
+//     // private String gtype;
+//     // private String thumbnail;   //URL
+//     // private String video;       //URL
+//     // private String gameSrc;     //URL
+
+//     // public Game() {}
+
+//     // public Game 
+// }
