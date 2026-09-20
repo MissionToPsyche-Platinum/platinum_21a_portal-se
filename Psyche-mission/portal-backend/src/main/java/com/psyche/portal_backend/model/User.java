c9b74c10f8bc5b08ef01f3dfe3300e808ebe427a
aghines <aghines@asu.edu>
2026-09-19 18:20:13 -0700

    Created User entity for backend/db use

--- Psyche-mission/portal-backend/src/main/java/com/psyche/portal_backend/model/User.java
+++ Psyche-mission/portal-backend/src/main/java/com/psyche/portal_backend/model/User.java
@@ -1,22 +1,33 @@
 package com.psyche.portal_backend.model;
 
 import java.util.concurrent.atomic.AtomicInteger;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name="users")
 public class User {
-    private static final AtomicInteger idGenerator = new AtomicInteger(1);
-    private final int userId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Db will generate id
+    private Long userId;
+
+    @Column(nullable=false, unique=true)
     private String username;
+
+    @Column(nullable=false, unique=true)
     private String email;
+
+    @Column(nullable=false)
     private String password;
 
+    public User() {}
+
     public User(String username, String email, String password) {
-        this.userId = idGenerator.getAndIncrement();
         this.username = username;
         this.email = email;
         this.password = password;
     }
 
-    public int getUserId() {
+    public Long getUserId() {
         return this.userId;
     }
 
