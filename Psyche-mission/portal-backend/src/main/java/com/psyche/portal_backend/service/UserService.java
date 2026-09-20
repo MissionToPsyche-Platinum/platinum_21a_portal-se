409112aab57dd16b29af7b2196437a7ea977f8ec
aghines <aghines@asu.edu>
2026-09-19 18:36:42 -0700

    Created userrepo and userservice

---
+++ Psyche-mission/portal-backend/src/main/java/com/psyche/portal_backend/service/UserService.java
@@ -0,0 +1,25 @@
+package com.psyche.portal_backend.service;
+
+import com.psyche.portal-backend.model.User;
+import com.psyche.portal-backend.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User getUserById(Long id) {
+        Optional<User> user = userRepository.findById(id);
+
+        if (user.isEmpty()) {
+            throw new RuntimeException("User not found");
+        }
+
+        return user.get();
+    }
+}
